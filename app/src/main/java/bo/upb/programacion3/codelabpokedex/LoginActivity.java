1b5e721188afd33f332a1cd48a7d609a9a8003c3
Lucia Paredes <lparedes15@isc.lp.upb.edu>
2019-12-03 23:26:00 -0400

    pokemon

---
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/LoginActivity.java
@@ -0,0 +1,15 @@
+package bo.upb.programacion3.codelabpokedex;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+}
---
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/model/User.java
@@ -0,0 +1,27 @@
+package bo.upb.programacion3.codelabpokedex.model;
+
+public class User {
+    private String user;
+    private String password;
+    private int image;
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword() {
+        this.password = password;
+    }
+}
