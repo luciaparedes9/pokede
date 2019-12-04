dafa1de301a8a372e52cfba9628188a5c789c715
Lucia Paredes <lparedes15@isc.lp.upb.edu>
2019-12-03 23:24:15 -0400

    pokemon

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/MainActivity.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/MainActivity.java
@@ -1,5 +1,6 @@
 package bo.upb.programacion3.codelabpokedex;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
@@ -39,9 +40,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void goToMyPokemons(View view) {
+        Intent intent = new Intent(this, PokemonRecyclerActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMyFavoritePokemons(View view) {
+        Intent intent = new Intent(this, PokemonListActivity.class);
+        startActivity(intent);
+    }
+
     // Esto ya saben!
     private void initViews() {
         this.toolbar = findViewById(R.id.toolbar);
         this.drawerLayout = findViewById(R.id.navigationDrawer);
     }
+
+
 }
--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -27,6 +27,42 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+
+        pokemons.add(new Pokemon(13, "Caterpie", R.drawable.caterpie, "Bug"));
+        pokemons.add(new Pokemon(14, "Charizard", R.drawable.charizard, "Fire"));
+        pokemons.add(new Pokemon(15, "Dragonite", R.drawable.dragonite, "Flying"));
+        pokemons.add(new Pokemon(16, "Eevee", R.drawable.eevee, "Normal"));
+        pokemons.add(new Pokemon(17, "Haunter", R.drawable.hunter, "Ghost"));
+        pokemons.add(new Pokemon(18, "Jigglypuff", R.drawable.jiggly, "Normal"));
+        pokemons.add(new Pokemon(19, "Psyduck", R.drawable.psyduck, "Water"));
+        pokemons.add(new Pokemon(20, "Seviper", R.drawable.seviper, "Poison"));
+        pokemons.add(new Pokemon(21, "Wobbuffet", R.drawable.wob, "Psychic"));
+        pokemons.add(new Pokemon(22, "Wooloo", R.drawable.wooloo, "Normal"));
+
         return pokemons;
+    }
+
+    public static List<Pokemon> AshFavorites() {
+        List<Pokemon> ashPokemon = new ArrayList<>();
+        ashPokemon.add(new Pokemon(1, "Pikachu", R.drawable.pikachu, "Electric"));
+        ashPokemon.add(new Pokemon(14, "Charizard", R.drawable.charizard, "Flying"));
+        ashPokemon.add(new Pokemon(4, "Squirtle", R.drawable.squirtle, "Water"));
+        return ashPokemon;
+    }
+
+    public static List<Pokemon> MistyFavorites() {
+        List<Pokemon> mistyPokemon = new ArrayList<>();
+        mistyPokemon.add(new Pokemon(19, "Psyduck", R.drawable.psyduck, "Water"));
+        mistyPokemon.add(new Pokemon(16, "Eevee", R.drawable.eevee, "Normal"));
+        mistyPokemon.add(new Pokemon(13, "Caterpie", R.drawable.caterpie, "Bug"));
+        return mistyPokemon;
+    }
+
+    public static List<Pokemon> JessyFavorites() {
+        List<Pokemon> jessyPokemon = new ArrayList<>();
+        jessyPokemon.add(new Pokemon(20, "Seviper", R.drawable.seviper, "Poison"));
+        jessyPokemon.add(new Pokemon(7, "Ekans", R.drawable.ekans, "Poison"));
+        jessyPokemon.add(new Pokemon(21, "Wobbuffet", R.drawable.wob, "Psychic"));
+        return jessyPokemon;
     }
 }
